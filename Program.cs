e3935d0cafaa0211f4c02536cdfc3b74131e71a5
Paul <paul18346@gmail.com>
2020-02-23 23:46:38 -0700

    Un fizz Buzz mas complicado

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+
+
+namespace Fizz_Buzz__
+
+{
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+             for(int Numero = 1; Numero < 100; Numero++)
+
+            {
+
+                
+
+                
+
+                 if((Numero % 3) == 0 )
+
+                 Console.Write("Fizz");
+
+            
+
+                 else
+
+                  if((Numero % 5) == 0 )
+
+                  Console.Write("Buzz");
+
+                  
+
+                  else
+
+                   Console.WriteLine(Numero);
+
+            }
+
+        }
+
+    }
+
+}
